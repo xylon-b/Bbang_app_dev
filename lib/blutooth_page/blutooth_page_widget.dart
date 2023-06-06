3786c2a339a4dc95aa8c57bfe32cda6fe2f8e8e7
백창현 <ckdgus1373@naver.com>
2023-06-06 21:42:00 +0900

    블루투스
    블루투스

--- lib/blutooth_page/blutooth_page_widget.dart
+++ lib/blutooth_page/blutooth_page_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -7,6 +8,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'blutooth_page_model.dart';
 export 'blutooth_page_model.dart';
+import 'package:flutter/material.dart';
+import 'device_screen.dart';
 
 class BlutoothPageWidget extends StatefulWidget {
   const BlutoothPageWidget({Key? key}) : super(key: key);
@@ -17,6 +20,9 @@ class BlutoothPageWidget extends StatefulWidget {
 
 class _BlutoothPageWidgetState extends State<BlutoothPageWidget> {
   late BlutoothPageModel _model;
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<ScanResult> scanResultList = [];
+  bool _isScanning = false;
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final _unfocusNode = FocusNode();
@@ -24,7 +30,93 @@ class _BlutoothPageWidgetState extends State<BlutoothPageWidget> {
   @override
   void initState() {
     super.initState();
+    initBle();
     _model = createModel(context, () => BlutoothPageModel());
+  }
+
+  void initBle() {
+    flutterBlue.isScanning.listen((isScanning) {
+      _isScanning = isScanning;
+      setState(() {});
+    });
+  }
+
+  scan() async {
+    if (!_isScanning) {
+      scanResultList.clear();
+      flutterBlue.startScan(timeout: Duration(seconds: 4));
+      flutterBlue.scanResults.listen((results) {
+        setState(() {
+          scanResultList = results;
+        });
+      });
+      setState(() {
+        _isScanning = true;
+      });
+    } else {
+      flutterBlue.stopScan();
+      setState(() {
+        _isScanning = false;
+      });
+    }
+  }
+
+  Widget deviceSignal(ScanResult r) {
+    return Text(r.rssi.toString());
+  }
+
+  /* 장치의 MAC 주소 위젯  */
+  Widget deviceMacAddress(ScanResult r) {
+    return Text(r.device.id.id);
+  }
+
+  /* 장치의 명 위젯  */
+  Widget deviceName(ScanResult r) {
+    String name = '';
+
+    if (r.device.name.isNotEmpty) {
+      // device.name에 값이 있다면
+      name = r.device.name;
+    } else if (r.advertisementData.localName.isNotEmpty) {
+      // advertisementData.localName에 값이 있다면
+      name = r.advertisementData.localName;
+    } else {
+      // 둘다 없다면 이름 알 수 없음...
+      name = 'N/A';
+    }
+    return Text(name);
+  }
+
+  /* BLE 아이콘 위젯 */
+  Widget leading(ScanResult r) {
+    return CircleAvatar(
+      child: Icon(
+        Icons.bluetooth,
+        color: Colors.white,
+      ),
+      backgroundColor: Colors.cyan,
+    );
+  }
+
+  /* 장치 아이템을 탭 했을때 호출 되는 함수 */
+  void onTap(ScanResult r) {
+    // 단순히 이름만 출력
+    print('${r.device.name}');
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => DeviceScreen(device: r.device)),
+    );
+  }
+
+  /* 장치 아이템 위젯 */
+  Widget listItem(ScanResult r) {
+    return ListTile(
+      onTap: () => onTap(r),
+      leading: leading(r),
+      title: deviceName(r),
+      subtitle: deviceMacAddress(r),
+      trailing: deviceSignal(r),
+    );
   }
 
   @override
@@ -112,15 +204,23 @@ class _BlutoothPageWidgetState extends State<BlutoothPageWidget> {
                   ],
                 ),
               ),
-              Container(
-                width: double.infinity,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  color: FlutterFlowTheme.of(context).secondaryBackground,
+              Expanded(
+                child: ListView.separated(
+                  itemCount: scanResultList.length,
+                  itemBuilder: (context, index) {
+                    return listItem(scanResultList[index]);
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Divider();
+                  },
                 ),
               ),
             ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: scan,
+          child: Icon(_isScanning ? Icons.stop : Icons.search),
         ),
       ),
     );
